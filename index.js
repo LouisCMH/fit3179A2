ca3fb495b593b277f1c0d41b93770ee1322e29da
LouisCMH <lcho0031@student.monash.edu>
2024-10-15 21:19:55 +0800

    Added assignment files for submission

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 var spec1 = "vegalite_files/malaysia_map.vg.json";
 var spec2 = "vegalite_files/poverty.vg.json";
-var spec3 = "vegalite_files/demographic.vg.json";
-vegaEmbed('#choropleth_map', spec1).then(function(result) {
+var spec3 = "vegalite_files/pyramid_penang.vg.json";
+var spec4 = "vegalite_files/pyramid_kl.vg.json";
+var spec5 = "vegalite_files/pyramid_johor.vg.json";
+var spec6 = "vegalite_files/bump.vg.json";
+var spec7 = "vegalite_files/heatmap.vg.json";
+vegaEmbed('#choropleth_map', spec1, { actions: false }).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed('#poverty_graph', spec2).then(function(result) {
+vegaEmbed('#poverty_graph', spec2, { actions: false }).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed('#demographic_heatmap', spec3).then(function(result) {
+vegaEmbed('#pyramid_penang', spec3, { actions: false }).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-// vegaEmbed('#choropleth_map_2', spec2).then(function(result) {
-//   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-// }).catch(console.error);
-// vegaEmbed('#plot_1', spec2).then(function(result) {
-//   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-// }).catch(console.error);
+vegaEmbed('#pyramid_kl', spec4, { actions: false }).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed('#pyramid_johor', spec5, { actions: false }).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed('#bump', spec6, { actions: false }).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed('#heat', spec7, { actions: false }).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
